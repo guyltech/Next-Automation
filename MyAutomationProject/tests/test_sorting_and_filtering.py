467b183261da3062064ffef5dd6007e8ad776baa
Guy Levy <guylevi811@gmail.com>
2025-05-01 19:13:26 +0300

    Delete MyAutomationProject directory

--- MyAutomationProject/tests/test_sorting_and_filtering.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from tests.base_test import BaseTest
-
-@pytest.mark.usefixtures("setup_page_function")
-class TestSortingAndFiltering(BaseTest):
-
-    # ! Implement test_navigation_header first !
-    def test_sort_beds_most_popular(self) -> None:
-        pass
