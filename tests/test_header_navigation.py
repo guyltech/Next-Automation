ee97554701058357e304f3d22035292eee071cf0
Guy Levy <guylevi811@gmail.com>
2025-05-01 19:17:07 +0300

    Add files via upload

--- tests/test_header_navigation.py
+++ tests/test_header_navigation.py
@@ -17,10 +17,19 @@ class TestHeaderNavigation(BaseTest):
         #assert "coatsandjackets" in self.search_results_page.url()
 
     # TC03: Verify clicking the 'Baby' category navigates the user to the corresponding category page
-
+    def test_click_baby_department_navigates_correctly(self) -> None:
+        self.home_page.common_dialogs.close_accept_cookies_and_country_dialog()
+        self.home_page.select_main_department_and_sub_category("Baby", "")
+        #assert "baby" in self.search_results_page.url()
 
     # TC04: Verify clicking the subcategory 'First Size' under the 'Baby' category navigates the user to the corresponding category page
+    def test_click_subcategory_baby_first_size_navigates_correctly(self) -> None:
+        self.home_page.common_dialogs.close_accept_cookies_and_country_dialog()
+        self.home_page.select_main_department_and_sub_category("Baby","First Size")
+        #assert "firstsize" in self.search_results_page.url()
 
-
-    # TC05: Verify clicking the subcategory 'GHD' under the 'Beauty' category navigates the user to the corresponding category page
-
+    # TC05: Verify clicking the subcategory 'Gifts For Her' under the 'Beauty' category navigates the user to the corresponding category page
+    def test_click_subcategory_beauty_gifts_for_her_navigates_correctly(self) -> None:
+        self.home_page.common_dialogs.close_accept_cookies_and_country_dialog()
+        self.home_page.select_main_department_and_sub_category("Beauty","Gifts For Her")
+        #assert "giftsforher" in self.search_results_page.url()
